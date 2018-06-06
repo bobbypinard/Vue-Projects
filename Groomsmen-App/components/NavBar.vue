be28e53fa3b74910aa38875d507d9849aa5b96fc
Bobby Pinard <bobby.pinard@gmail.com>
2018-06-05 21:34:18 -0500

    Added Navbar component to app.

---
+++ Groomsmen-App/components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="nav">
+    <div v-for="item in items" :key="item.title" class="item">
+      <span >{{ item.title }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        {title: 'This is the title.'}
+      ]
+    }
+  }
+}
+</script>
