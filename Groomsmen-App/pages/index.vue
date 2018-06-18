30184f8935fd3616b45a37b35faf05679838bccc
Bobby Pinard <bobby.pinard@gmail.com>
2018-06-17 21:23:06 -0500

    Remover unneeded code.

--- Groomsmen-App/pages/index.vue
+++ Groomsmen-App/pages/index.vue
@@ -5,22 +5,11 @@
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
 import Todo from '~/components/Todo.vue'
 
 export default {
   components: {
-    AppLogo,
     Todo
   }
 }
 </script>
-
-<style>
-body {
-  background-image: url("/assets/background.jpg");
-  background-attachment: fixed;
-  background-position: center;
-  background-repeat: no-repeat;
-}
-</style>
