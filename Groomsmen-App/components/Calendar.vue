1be390d44a9d43ca6801c0ae29c7a809f1e78fb6
Bobby Pinard <bobby.pinard@gmail.com>
2018-06-17 21:41:16 -0500

    Added Calendar component.

---
+++ Groomsmen-App/components/Calendar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="Calendar">
+    <h1 class="date">{{ date }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      date: 'May 3'
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+h1.date {
+  font-family: 'Dancing Script';
+}
+</style>
