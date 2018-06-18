ef291b04204a2d81d1eebc3c24bddfe544fe1a24
Bobby Pinard <bobby.pinard@gmail.com>
2018-06-17 21:23:40 -0500

    Added Schdule view.

---
+++ Groomsmen-App/pages/Schedule.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="Schedule">
+    <calendar />
+  </div>
+</template>
+
+<script>
+import Calendar from '~/components/Calendar.vue'
+
+export default {
+  components: {
+    Calendar
+  }
+}
+</script>
