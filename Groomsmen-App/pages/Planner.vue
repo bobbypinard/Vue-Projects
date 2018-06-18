f87ae9241cef5945730a8c8edc1a66fb1ae97cf8
Bobby Pinard <bobby.pinard@gmail.com>
2018-06-17 21:23:20 -0500

    Added Planner view.

---
+++ Groomsmen-App/pages/Planner.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="Planner">
+    <h1>Planner</h1>
+    <todo />
+  </div>
+</template>
+
+<script>
+import Todo from '~/components/Todo.vue'
+
+export default {
+  components: {
+    Todo
+  }
+}
+</script>
+
+<style>
+h1 {
+  text-align: center;
+  font-family: 'Great Vibes';
+  font-size: 4em;
+  margin-bottom: 2em;
+  border-bottom: 2px solid #ccac00;
+}
+.Planner {
+}
+</style>
