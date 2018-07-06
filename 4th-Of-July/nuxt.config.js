6bd01ae213201eca48b1d9f3a8e838537da736a5
Bobby Pinard <bobby.pinard@gmail.com>
2018-07-05 19:57:41 -0500

    Removed starter template code.

--- 4th-Of-July/nuxt.config.js
+++ 4th-Of-July/nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'fourth',
+    title: '4th Of July',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
