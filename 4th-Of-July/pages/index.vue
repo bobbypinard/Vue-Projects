2463847db76045c5bee216bdc9a99efab97281cb
Bobby Pinard <bobby.pinard@gmail.com>
2018-07-06 22:01:17 -0500

    Added stripes to flag.

--- 4th-Of-July/pages/index.vue
+++ 4th-Of-July/pages/index.vue
@@ -1,5 +1,42 @@
 <template>
   <div class="container">
-    
+    <div class="flag">
+      <div class="stripe" />
+      <div class="stripe" />
+      <div class="stripe" />
+      <div class="stripe" />
+      <div class="stripe" />
+      <div class="stripe" />
+      <div class="stripe" />
+      <div class="stripe" />
+      <div class="stripe" />
+      <div class="stripe" />
+      <div class="stripe" />
+      <div class="stripe" />
+      <div class="stripe" />
+    </div>
   </div>
 </template>
+
+<style scoped>
+.container {
+  background-color: rgb(158, 210, 255);
+  min-height: 100vh;
+}
+
+.flag {
+  height: 26em;
+  width: 45em;
+  background-color: white;
+  margin: auto;
+  display: grid;
+  grid-template-rows: repeat(2, 1fr);
+  grid-template-columns: 1fr, 3fr;
+  grid-template-areas: 
+}
+
+.stripe {
+  height: 2em;
+  background-color: red;
+}
+</style>
