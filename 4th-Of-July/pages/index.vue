4568691e3da9331f522431385f7b408c33d4ed8c
Bobby Pinard <bobby.pinard@gmail.com>
2018-07-07 18:24:24 -0500

    Stripes and star area working.

--- 4th-Of-July/pages/index.vue
+++ 4th-Of-July/pages/index.vue
@@ -1,19 +1,24 @@
 <template>
   <div class="container">
-    <div class="flag">
-      <div class="stripe" />
-      <div class="stripe" />
-      <div class="stripe" />
-      <div class="stripe" />
-      <div class="stripe" />
-      <div class="stripe" />
-      <div class="stripe" />
-      <div class="stripe" />
-      <div class="stripe" />
-      <div class="stripe" />
-      <div class="stripe" />
-      <div class="stripe" />
-      <div class="stripe" />
+    <div class="flag stripes">
+      <div class="stars">
+        <div class="star" />
+      </div>
+
+      <div class="star-stripe"></div>
+      <div class="star-tripe"></div>
+      <div class="star-stripe"></div>
+      <div class="star-tripe"></div>
+      <div class="star-stripe"></div>
+      <div class="star-tripe"></div>
+      <div class="star-stripe"></div>
+
+      <div class="tripe"></div>
+      <div class="stripe"></div>
+      <div class="tripe"></div>
+      <div class="stripe"></div>
+      <div class="tripe"></div>
+      <div class="stripe"></div>
     </div>
   </div>
 </template>
@@ -25,18 +30,36 @@
 }
 
 .flag {
-  height: 26em;
+  height: 24em;
   width: 45em;
   background-color: white;
   margin: auto;
   display: grid;
-  grid-template-rows: repeat(2, 1fr);
-  grid-template-columns: 1fr, 3fr;
-  grid-template-areas: 
+  grid-template-columns: 2fr 3fr;
+}
+
+.stripes {
+  display: grid;
+  grid-template-rows: repeat(13, 1fr);
 }
 
 .stripe {
-  height: 2em;
+  background-color: red;
+  grid-column: 1 / span 2;
+}
+
+.stars {
+  grid-column: 1;
+  grid-row: 1 / span 7;
+  background: blue;
+
+  display: grid;
+  grid-template-columns: repeat(9, 1fr)
+}
+
+.star-stripe {
+  grid-column: 2;
   background-color: red;
 }
+
 </style>
